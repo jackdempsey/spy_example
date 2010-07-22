5c3864be3e8e93164301553c4353b719a9e4c7f4
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-22 16:19:17 -0400

    remove files handled by engine

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,4 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
-  def is_admin?
-    email == 'admin@test.com'
-  end
-
-  def to_s
-    email
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 SignInMiddlewareExample::Application.routes.draw do |map|
   devise_for :users
 
-  match 'user_switcher' => 'users#user_switcher'
-
-  match 'sign_in_admin' => 'users#sign_in_admin'
-
-  match 'sign_out_admin' => 'users#sign_out_admin'
-
   resources :users
 
   # Sample resource route with options:
