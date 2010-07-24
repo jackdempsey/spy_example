2d28478ed367ab84fd61c35efe94de04cd21d7b3
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-24 14:31:30 -0400

    put these methods back into responsibility of app

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def is_admin?
+    email == 'admin@test.com'
+  end
+
+  def to_s
+    email
+  end
+
 end
