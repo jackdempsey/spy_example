2bc28381e07b5a02bba39e97daa2576b5ac2f2fc
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-24 13:23:22 -0400

    remove clutter

--- config/routes.rb
+++ config/routes.rb
@@ -3,33 +3,6 @@ SignInMiddlewareExample::Application.routes.draw do |map|
 
   resources :users
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
   root :to => "home#index"
 
 end
